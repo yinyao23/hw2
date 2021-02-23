737377ec9a5db1bb3212828d5f1f2c411da0c0e1
EC2 Default User <ec2-user@ip-172-31-7-236.ec2.internal>
2021-02-23 20:40:03 +0000

    first commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+    # def self.with_ratings(ratings_list)
+    #     if ratings_list = nil
+    #         return Movie.where()
+    #     return Movie.where(rating: ratings_list)
+        
+    # end
 end
